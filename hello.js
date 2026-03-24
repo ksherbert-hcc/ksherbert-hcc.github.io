8a467a91f53651022ed21c4dce7cb134b9e2c4cb
Kyle Sherbert <ksherbert@hcc-nd.edu>
2026-03-23 22:11:43 -0400

    Comment up hello.js so it is suitable for student consumption

--- hello.js
+++ hello.js
@@ -1,6 +1,12 @@
+// Good code includes lots of comments, to guide the reader in understanding what is going on.
+// You should write good code. You should write comments!
 
-
-
+/*
+ * Prompt the user to start a game.
+ *
+ * Parameters:
+ * - user: a string with the user's name
+ */
 function startgame(user) {
     console.log("Hello, "+user+". Would you like to play a game?");
     console.log("1. Yes");
@@ -10,18 +16,21 @@ function startgame(user) {
     let choice = prompt("Enter your choice:");
     switch (choice) {
         case "1":
-            woodedPath(user);
+            woodedPath(user);                                   // Change the scene.
             return;
         case "2":
             console.log("You walk away without playing.");
-            endgame();
+            endgame();                                          // Ending 1.
             return;
         default:
             console.log(choice+" is not valid. Please enter a number corresponding to your choice.");
-            startgame(user);
+            startgame(user);                                    // Try the same scene again.
     }
 }
 
+/*
+ * Display a consistent end game dialogue.
+ */
 function endgame() {
     console.log("");
     console.log("Game over. Refresh to play again.");
@@ -29,6 +38,16 @@ function endgame() {
     console.log("This was one of 5 unique endings. Try to find them all.");
 }
 
+
+/*
+ * Change the scene to the wooded path.
+ *
+ * Parameters:
+ * - user: a string with the user's name
+ * - hasSword: a boolean tracking whether the player has found the sword
+ * - killedOgre: a boolean tracking whether the player has slain the ogre
+ * - hasPrincess: a boolean tracking whether the player has rescued the princess
+ */
 function woodedPath(user, hasSword, killedOgre, hasPrincess) {
     console.log("You are in the woods, following a path to the enchanted castle.");
     console.log("1. Look around.");
@@ -44,10 +63,10 @@ function woodedPath(user, hasSword, killedOgre, hasPrincess) {
             } else {
                 console.log("You find a sword embedded in a tree. You pull it out.");
             }
-            woodedPath(user, true, killedOgre, hasPrincess);
+            woodedPath(user, true, killedOgre, hasPrincess);            // Refresh the scene, having found the sword.
             return;
         case "2":
-            castleGate(user, hasSword, hasPrincess);
+            castleGate(user, hasSword, hasPrincess);                    // Change the scene.
             return;
         case "3":
             if (hasPrincess) {
@@ -55,14 +74,23 @@ function woodedPath(user, hasSword, killedOgre, hasPrincess) {
             } else {
                 console.log("You go home. The princess is never seen or heard from again, and you are ridiculed as a coward.");
             }
-            endgame();
+            endgame();                                                  // Ending 2 or 5.
             return;
         default:
             console.log(choice+" is not valid. Please enter a number corresponding to your choice.");
-            woodedPath(user, true, killedOgre, hasPrincess);
+            woodedPath(user, true, killedOgre, hasPrincess);            // Try the same scene again.
     }
 }
 
+/*
+ * Change the scene to the castle gate.
+ *
+ * Parameters:
+ * - user: a string with the user's name
+ * - hasSword: a boolean tracking whether the player has found the sword
+ * - killedOgre: a boolean tracking whether the player has slain the ogre
+ * - hasPrincess: a boolean tracking whether the player has rescued the princess
+ */
 function castleGate(user, hasSword, killedOgre, hasPrincess) {
     if (killedOgre) {
         console.log("You are at the castle gate. The ogre lies dead at your feet.");
@@ -72,8 +100,8 @@ function castleGate(user, hasSword, killedOgre, hasPrincess) {
         console.log("");
     } else if (hasPrincess) {
         console.log("You try to sneak back out, but the princess shrieks when she sees the ogre, and he eats you both.");
-        endgame();
-        return;
+        endgame();                  // Ending 4.
+        return;                     // Terminate the game without prompting the user for the menu in the latter half of this function.
     } else {
         console.log("You are at the castle gate. It is guarded by a fearsome ogre.");
         console.log("1. Fight the ogre.");
@@ -87,27 +115,36 @@ function castleGate(user, hasSword, killedOgre, hasPrincess) {
         case "1":
             if (killedOgre) {
                 console.log("The corpse is already starting to stink. Best leave it alone.");
-                castleGate(user, hasSword, killedOgre, hasPrincess);
+                castleGate(user, hasSword, killedOgre, hasPrincess);    // Refresh scene.
             } else if (hasSword) {
                 console.log("You slay the ogre with your trusty sword.");
-                castleGate(user, hasSword, true, hasPrincess);
+                castleGate(user, hasSword, true, hasPrincess);          // Refresh scene, having slain the ogre.
             } else {
                 console.log("Without a sword, you are no match for the ogre. It eats you.");
-                endgame();
+                endgame();                                              // Ending 3.
             }
             return;
         case "2":
-            throneRoom(user, hasSword, killedOgre, hasPrincess);
+            throneRoom(user, hasSword, killedOgre, hasPrincess);        // Change scene.
             return;
         case "3":
-            woodedPath(user, hasSword, killedOgre, hasPrincess);
+            woodedPath(user, hasSword, killedOgre, hasPrincess);        // Change scene.
             return;
         default:
             console.log(choice+" is not valid. Please enter a number corresponding to your choice.");
-            castleGate(user, true, killedOgre, hasPrincess);
+            castleGate(user, true, killedOgre, hasPrincess);            // Try the same scene again.
     }
 }
 
+/*
+ * Change the scene to the throne room.
+ *
+ * Parameters:
+ * - user: a string with the user's name
+ * - hasSword: a boolean tracking whether the player has found the sword
+ * - killedOgre: a boolean tracking whether the player has slain the ogre
+ * - hasPrincess: a boolean tracking whether the player has rescued the princess
+ */
 function throneRoom(user, hasSword, killedOgre, hasPrincess) {
     if (hasPrincess) {
         console.log("The throne room is now quiet.");
@@ -120,11 +157,11 @@ function throneRoom(user, hasSword, killedOgre, hasPrincess) {
     let choice = prompt("Enter your choice:");
     switch (choice) {
         case "1":
-            castleGate(user, hasSword, killedOgre, true);
+            castleGate(user, hasSword, killedOgre, true);           // Change scene, having found the princess.
             return;
         default:
             console.log(choice+" is not valid. Please enter a number corresponding to your choice.");
-            throneRoom(user, true, killedOgre, hasPrincess);
+            throneRoom(user, hasSword, killedOgre, hasPrincess);    // Try the same scene again.
     }
 }
 
